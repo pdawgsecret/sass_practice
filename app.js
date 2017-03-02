7911c2296e5de38216da48a5062a6f3b7f47fe79
piercebelton <piercebelton@gmail.com>
2017-03-02 15:06:35 -0800

    fix some styling

---
+++ app.js
@@ -0,0 +1,21 @@
+var images = {
+	man: {
+		heading: 'Man Image',
+		description: 'Image of a man.',
+		path: 'img/man.jpg'
+	},
+	wizard: {
+		heading: 'Wizard Image',
+		description: 'Image of a wizard.',
+		path: 'img/wizard.jpg'
+	},
+	beast: {
+		heading: 'Beast Image',
+		description: 'Image of a beast.',
+		path: 'img/beast.jpg'
+	}
+};
+
+$(document).ready(function() {
+
+});
